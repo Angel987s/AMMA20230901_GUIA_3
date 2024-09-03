ed47361b9672c4783ab00cb671a4f426e482dbb4
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-09-02 20:41:58 -0600

    AccountController terminado

---
+++ AMMA20230901/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AMMA20230901.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpPost("login")]
+        public IActionResult Login(string login, string password)
+        {
+            // Comprueba si las credenciales son válidas
+            if (login == "admin" && password == "12345")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, login)
+                };
+                var claimsIdentity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties
+                {
+                    // Puedes configurar propiedades adicionales aquí
+                    // Por ejemplo, para hacer que la cookie persista:
+                    IsPersistent = true,
+                    // Para especificar una fecha de expiración:
+                    ExpiresUtc = DateTime.UtcNow.AddDays(7)
+                };
+                // Inicia sesión del usuario
+                HttpContext.SignInAsync(
+                   CookieAuthenticationDefaults.AuthenticationScheme,
+                   new ClaimsPrincipal(claimsIdentity), authProperties);
+                return Ok("Inició sesión correctamente.");
+            }
+            else
+            {
+                return Ok("Credenciales Incorrectas.");
+
+            }
+        }
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            // Cierra la sesión del usuarios
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Devuelve una respuesta exitosa
+            return Ok("Cerró sesión correctamente.");
+        }
+    }
+}
--- AMMA20230901/Program.cs
+++ AMMA20230901/Program.cs
@@ -1,11 +1,44 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Add services to the container.
+
+// Configuración para la autenticación por cookie
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        // Configura el nombre del parámetro de URL para redireccionamiento no autorizado
+        options.ReturnUrlParameter = "unauthorized";
+
+        options.Events = new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = context =>
+            {
+                // Cambia el código de estado a No autorizado
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+
+                // Establece el tipo de contenido como JSON (u otro formato deseado)
+                context.Response.ContentType = "application/json";
+
+                var message = new
+                {
+                    error = "No autorizado",
+                    message = "Debe iniciar sesión para acceder a este recurso."
+                };
+
+                // Serializa el objeto 'message' en formato JSON
+                var jsonMessage = JsonSerializer.Serialize(message);
+
+                // Escribe el mensaje JSON en la respuesta HTTP
+                return context.Response.WriteAsync(jsonMessage);
+            }
+        };
+    });
 
 var app = builder.Build();
 
@@ -19,7 +52,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
